63bb1969e0084ef63b218c21204e19d335883193
Igor Vieira de Mello <igor@shapeweb.com.br>
2021-02-23 09:23:39 -0400

    test static variables

--- libs/db.tsx
+++ libs/db.tsx
@@ -2,11 +2,16 @@
 import mysql from 'serverless-mysql';
 const db = mysql({
   config: {
-    host: process.env.MYSQL_HOST,
-    port: Number(process.env.MYSQL_PORT),
-    database: process.env.MYSQL_DATABASE,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD
+    // host: process.env.MYSQL_HOST,
+    // port: Number(process.env.MYSQL_PORT),
+    // database: process.env.MYSQL_DATABASE,
+    // user: process.env.MYSQL_USER,
+    // password: process.env.MYSQL_PASSWORD
+    host: '165.227.68.74',
+    port: 3306,
+    database: 'nextjs-external',
+    user: 'shapeweb2',
+    password: 'sh615243'
   }
 });
 export async function query({ query, values }) {
