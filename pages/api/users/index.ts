c25311af6f3f66c16ec4226227b8c70aaf5d6ec0
Igor Vieira de Mello <igor@shapeweb.com.br>
2021-02-23 07:55:21 -0400

    users select with scores

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -4,8 +4,10 @@ import { query } from '../../../libs/db';
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const result = await query({
     query: `
-        SELECT u.*
-        FROM users u
+      SELECT u.*, SUM(s.total) as total, s.price_of_day as price_of_day
+      FROM users u
+      LEFT JOIN scores s ON s.user_id = u.id
+      GROUP BY u.id
     `,
     values: [],
   });
