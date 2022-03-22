6f81f232a23e0f49c82834e70b2184bec133b898
Igor Mello <igor.mello@madeiramadeira.com.br>
2022-03-22 11:25:58 -0300

    teste

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
-import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
-import { PriceContextProvider } from '../contexts/PriceContext';
+import "../styles/globals.scss";
+import type { AppProps } from "next/app";
+import { PriceContextProvider } from "../contexts/PriceContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <PriceContextProvider>
       <Component {...pageProps} />
     </PriceContextProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
